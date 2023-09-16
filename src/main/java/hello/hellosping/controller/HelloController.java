c1b54b41571acd6465e2c89852a80536e7731fde
DESKTOP-L7L63RK\mnyu1 <mnyu@cdssolution.co.kr>
2023-09-17 00:21:16 +0900

    spirngBoot Intro - section2

--- src/main/java/hello/hellosping/controller/HelloController.java
+++ src/main/java/hello/hellosping/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellosping.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -12,5 +14,38 @@ public class HelloController {
         model.addAttribute("data", "hello!!");
 
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMbc(@RequestParam(name = "name", required = false) String name, Model model){
+        model.addAttribute("name", name);
+
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello HelloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;           // json 형태로 전달 {"name":"yyyyyy"}
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
